ffca4a55f857be13570c329064529317de27c755
JoaoCarlosNogueira <joaocarlos2002_br@hotmail.com>
2024-10-15 09:14:40 -0400

    protegendo o template com scape

--- app/views/view.ts
+++ app/views/view.ts
@@ -2,15 +2,21 @@ import { Negociacoes } from "../models/negociacoes";
 
 export  abstract    class View<T>{
     protected elemento: HTMLElement;
-    
-    constructor(selector:string){
+    private escapar = false;
+    constructor(selector:string, escapar?: boolean){
       this.elemento = document.querySelector(selector);
+      if(escapar){
+        this.escapar = escapar;
+      }
     }
 
      protected abstract template (model: T): string;
 
       public update(model: T) : void {
-        const template = this.template(model);
+        let template = this.template(model);
+        if(this.escapar){
+          template = template.replace(/<script>[\s\S]*?<\/script>/,'')
+        }
         this.elemento.innerHTML = template;
     }
 }
